ebe4eb4cecdc20b8ddddd4b45d3fad5f9981be27
Juan Soto <borel.soto@gmail.com>
2026-05-07 20:23:56 -0300

    Criando projeto e repositorio git

---
+++ src/db/DB.java
@@ -0,0 +1,71 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+
+	// INICIAR CONEXAO
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	// ENCERRAR CONEXAO
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// CARREGAR AS INFOS DE CONEXAO
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) { // ABRE O ARQUIVO
+			Properties props = new Properties();
+			props.load(fs); // GUARDA OS DADOS DENTRO DO OBJ PROPERTIES
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
