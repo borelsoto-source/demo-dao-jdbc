3dfcd53129e1ad580d2ff6d19bbda9801e9a5460
Juan Soto <borel.soto@gmail.com>
2026-05-09 11:26:43 -0300

    Implements findByDepartment and findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-
+import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
@@ -14,6 +16,15 @@ public class Program {
 		System.out.println("=== Test 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("\n=== Test 2: seller findByDepartment ===");
+		Department department = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		list.forEach(System.out::println);
+		
+		System.out.println("\n=== Test 3: seller findAll ===");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
 		
 	}
 
