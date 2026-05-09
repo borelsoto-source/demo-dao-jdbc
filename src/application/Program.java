d08d4b21f47d1bf6cd40a33b354f874f61e2ac01
Juan Soto <borel.soto@gmail.com>
2026-05-08 21:46:00 -0300

    Reutilizando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,21 +3,16 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 
-import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		//Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3); 
 		
-		System.out.println(obj);
-		System.out.println();
+		System.out.println("=== Test 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 	}
